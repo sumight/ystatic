2bd1ce952a3f46def4b6ea572772a3645d3bae11
sumight <1005546504@qq.com>
2016-01-18 11:31:09 +0800

    增加端口配置

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ var koa = require('koa');
 var path = require('path');
 var app = koa();
 var root = path.join(__dirname, 'www');
+var config = require('./config.json');
+
 app.use(require('koa-static')(root));
 
-app.listen(3000);
+app.listen(config.port);
 
-console.log('listening on port 3000');
+console.log('listening on port '+config.port);
