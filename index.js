5856223d095530f4bd84b869141a38d927d449ba
sumight <1005546504@qq.com>
2016-11-29 21:18:05 +0800

    d

--- index.js
+++ index.js
@@ -1,13 +1,45 @@
+#!/usr/bin/env node
+
+
+var argv = require('yargs').argv;
 var koa = require('koa');
+var router = require('koa-router')();
 var path = require('path');
 var app = koa();
-var root = path.join(__dirname, 'www');
-var config = require('./config.json');
+var extend = require('extend');
+var fs = require('fs');
+// 默认配置
+defaultCfg = {
+    port:4000,
+    index: 'index.html',
+    root: './',
+    notfound: ''
+}
 
-app.use(require('koa-static')(root, {
-    index: config.index
+var cfg = extend({}, defaultCfg, argv);
+
+router.get('/**', function* (next) {
+    if(cfg.notfound === '') {
+        this.body = '<h1 style="text-align:center;margin-top:100px;">😢 Not Found </h1>';
+    }else {
+        this.body = fs.readFileSync(path.resolve(cfg.root, cfg.notfound)).toString();
+    }
+    yield next;
+})
+
+
+
+app.use(function *(next){
+    this.set('Access-Control-Allow-Origin','*');
+    yield next;
+});
+
+app.use(require('koa-static')(cfg.root , {
+    index: cfg.index
 }));
 
-app.listen(config.port);
+app.use(router.routes())
 
-console.log('listening on port '+config.port);
+app.listen(cfg.port);
+
+console.log('listening on port '+cfg.port);
