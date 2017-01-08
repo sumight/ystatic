c8d56bea06b1459974a30a31e953e708d286aa48
xiajiecheng <xiajiecheng@gmail.com>
2017-01-08 22:40:29 +0800

    feat: 添加非命令行调用的特性

--- index.js
+++ index.js
@@ -1,45 +1,44 @@
-#!/usr/bin/env node
-
-
-var argv = require('yargs').argv;
 var koa = require('koa');
 var router = require('koa-router')();
 var path = require('path');
 var app = koa();
 var extend = require('extend');
 var fs = require('fs');
-// 默认配置
-defaultCfg = {
-    port:4000,
-    index: 'index.html',
-    root: './',
-    notfound: ''
-}
 
-var cfg = extend({}, defaultCfg, argv);
-
-router.get('/**', function* (next) {
-    if(cfg.notfound === '') {
-        this.body = '<h1 style="text-align:center;margin-top:100px;">😢 Not Found </h1>';
-    }else {
-        this.body = fs.readFileSync(path.resolve(cfg.root, cfg.notfound)).toString();
+module.exports = function(argv) {
+    // 默认配置
+    defaultCfg = {
+        port: 8080,
+        index: 'index.html',
+        root: './',
+        notfound: ''
     }
-    yield next;
-})
+
+    var cfg = extend({}, defaultCfg, argv);
+
+    router.get('/**', function* (next) {
+        if(cfg.notfound === '') {
+            this.body = '<h1 style="text-align:center;margin-top:100px;">😢 Not Found </h1>';
+        }else {
+            this.body = fs.readFileSync(path.resolve(cfg.root, cfg.notfound)).toString();
+        }
+        yield next;
+    })
 
 
 
-app.use(function *(next){
-    this.set('Access-Control-Allow-Origin','*');
-    yield next;
-});
+    app.use(function *(next){
+        this.set('Access-Control-Allow-Origin','*');
+        yield next;
+    });
 
-app.use(require('koa-static')(cfg.root , {
-    index: cfg.index
-}));
+    app.use(require('koa-static')(cfg.root , {
+        index: cfg.index
+    }));
 
-app.use(router.routes())
+    app.use(router.routes())
 
-app.listen(cfg.port);
+    app.listen(cfg.port);
 
-console.log('listening on port '+cfg.port);
+    console.log('listening on port '+cfg.port);
+}
---
+++ static-cli.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+var argv = require('yargs').argv;
+var astatic = require('./index.js');
+
+astatic(argv);
