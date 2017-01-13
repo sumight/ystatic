342539f0222b398d7f309da682eb7f096b725d26
xiajiecheng <xiajiecheng@gmail.com>
2017-01-13 14:12:08 +0800

    feat: 添加内置功能接口
    - 图片上传接口， - 超时接口， - 错误接口

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 var koa = require('koa');
-var router = require('koa-router')();
+var _ = require('koa-route');
 var path = require('path');
+var parse = require('co-busboy');
 var app = koa();
 var extend = require('extend');
 var fs = require('fs');
+var source = require('vinyl-source-stream')
+var gulp = require('gulp')
+var timeout = require('koa-timeout');
 
 module.exports = function(argv) {
     // 默认配置
@@ -11,24 +15,26 @@ module.exports = function(argv) {
         port: 8080,
         index: 'index.html',
         root: './',
-        notfound: ''
+        notfound: '',
+        // 文件上传存储的位置
+        storage: './storage'
     }
 
     var cfg = extend({}, defaultCfg, argv);
 
-    router.get('/**', function* (next) {
+    app.use(function *(next){
+        this.set('Access-Control-Allow-Origin','*');
+        yield next;
+    });
+
+    // notfound页面
+    app.use(function *(next){
         if(cfg.notfound === '') {
             this.body = '<h1 style="text-align:center;margin-top:100px;">😢 Not Found </h1>';
         }else {
             this.body = fs.readFileSync(path.resolve(cfg.root, cfg.notfound)).toString();
         }
-        yield next;
-    })
 
-
-
-    app.use(function *(next){
-        this.set('Access-Control-Allow-Origin','*');
         yield next;
     });
 
@@ -36,7 +42,50 @@ module.exports = function(argv) {
         index: cfg.index
     }));
 
-    app.use(router.routes())
+    // 上传文件
+    app.use(_.post('/_upload', function *(next) {
+
+        // multipart upload
+        var parts = parse(this);
+        // 取第一个域
+        var part = yield parts;
+
+        part.pipe(source(part.filename))
+            .pipe(gulp.dest(path.resolve(cfg.root, cfg.storage)));
+
+        this.body = path.resolve('/', cfg.storage, part.filename);
+
+        yield next;
+    }));
+
+    // 超时请求
+    app.use(timeout(1e9));
+    app.use(_.get('/_timeout', function *(next) {
+        this.body ='timeout';
+        var time = parseInt(this.query.time);
+
+        // sleep
+        yield new Promise(function(resolve) {
+            setTimeout(()=>{
+                resolve();
+            }, isNaN(time)?1e9:time);
+        });
+
+        yield next;
+    }));
+
+    // 错误请求
+    app.use(_.get('/_error', function *(next) {
+        var status = parseInt(this.query.status);
+        this.throw(isNaN(status)?null:status);
+        yield next;
+    }));
+
+    // 列表数据请求
+
+    // 详情数据请求
+
+    // 异步表单校验
 
     app.listen(cfg.port);
 
